96fe4c4b9a9e2dc3dc6e8cb66e0b501dc6937c89
Binh Nguyen <b.nguyen.sn@gmail.com>
2019-11-01 22:15:53 +0000

    feat: update CLI package code

---
+++ packages/bundler-cli/README.md
@@ -0,0 +1,8 @@
+# Bundler CLI
+
+This is the CLI package for `@bnguyensn/bundler`. It is created on top of
+[Commander](https://github.com/tj/commander.js).
+
+## Options
+
+
--- packages/bundler-cli/index.js
+++ packages/bundler-cli/index.js
@@ -1,10 +1,15 @@
 #!/usr/bin/env node
 
+const path = require('path');
 const logger = require('@bnguyensn/logger')();
 const commander = require('commander');
+const cliPackageJson = require('./package');
 
 const nodeVer = process.version;
 
 logger.info(`You are running Node.js version ${nodeVer}`);
 
-const cli = new commander.Command();
+const cli = new commander.Command().version(cliPackageJson.version);
+
+const root = path.resolve(name);
+const appName = path.basename(root);
